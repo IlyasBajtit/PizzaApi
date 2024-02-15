0ebc23fb0d45e0fabeb1ac6e400fe8e18561595c
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-15 16:06:53 +0100

    feat: add models

---
+++ ApiBlazorPizza/Models/Ingredient.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiBlazorPizza.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PizzaId { get; set; }
+        public Pizza Pizza { get; set; }
+    }
+}
---
+++ ApiBlazorPizza/Models/Pizza.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiBlazorPizza.Models
+{
+    using System.Collections.Generic;
+
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ImageLink { get; set; }
+        public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
+    }
+}
