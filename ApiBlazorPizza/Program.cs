45141ac3b42b03d6e60d828e6abf24b7f214b98c
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-16 09:29:32 +0100

    feat: update program.cs

--- ApiBlazorPizza/Program.cs
+++ ApiBlazorPizza/Program.cs
@@ -1,11 +1,13 @@
 using ApiBlazorPizza;
+using ApiBlazorPizza.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddScoped<PanierService>();
+builder.Services.AddScoped<PizzaService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
