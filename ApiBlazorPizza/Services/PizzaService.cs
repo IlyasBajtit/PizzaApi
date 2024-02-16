cd21405667141d00fcaabaf6fdbf64006eef4816
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-16 09:29:04 +0100

    feat: add pizza service

--- ApiBlazorPizza/Services/PizzaService.cs
+++ ApiBlazorPizza/Services/PizzaService.cs
@@ -1,6 +1,46 @@
-﻿namespace ApiBlazorPizza.Services
+﻿using ApiBlazorPizza.Models;
+using System.Collections.Generic;
+using System.Linq;
+using ApiBlazorPizza.Data;
+
+namespace ApiBlazorPizza.Services
 {
     public class PizzaService
     {
+        // Récupérer toutes les pizzas depuis FakeDb
+        public IReadOnlyList<Pizza> GetPizzas() => FakeDb.Pizzas;
+
+        // La méthode AddPizza pourrait ajouter des pizzas directement à FakeDb
+        public void AddPizza(Pizza pizza)
+        {
+            FakeDb.Pizzas.Add(pizza);
+        }
+
+        // Récupérer une pizza par son ID depuis FakeDb
+        public Pizza GetPizzaById(int id) => FakeDb.Pizzas.FirstOrDefault(p => p.Id == id);
+
+        // Mettre à jour une pizza dans FakeDb
+        public void UpdatePizza(Pizza updatedPizza)
+        {
+            var pizzaIndex = FakeDb.Pizzas.FindIndex(p => p.Id == updatedPizza.Id);
+            if (pizzaIndex != -1)
+            {
+                FakeDb.Pizzas[pizzaIndex] = updatedPizza;
+            }
+            else
+            {
+                throw new KeyNotFoundException("Pizza not found");
+            }
+        }
+
+        // Supprimer une pizza de FakeDb
+        public void DeletePizza(int id)
+        {
+            var pizza = FakeDb.Pizzas.FirstOrDefault(p => p.Id == id);
+            if (pizza != null)
+            {
+                FakeDb.Pizzas.Remove(pizza);
+            }
+        }
     }
 }
