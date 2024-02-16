3bb40cc3c46626eadeb2594d9c07059acfe6cbd0
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-16 09:28:30 +0100

    feat: add Panier Service

--- ApiBlazorPizza/Services/PanierService.cs
+++ ApiBlazorPizza/Services/PanierService.cs
@@ -1,6 +1,45 @@
-﻿namespace ApiBlazorPizza.Services
+﻿using ApiBlazorPizza.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApiBlazorPizza.Services
 {
     public class PanierService
     {
+        private List<ArticlePanier> articles = new List<ArticlePanier>();
+
+        public IReadOnlyList<ArticlePanier> Articles => articles;
+
+        public void AjouterArticle(Pizza pizza, int quantite = 1)
+        {
+            var article = articles.FirstOrDefault(a => a.PizzaId == pizza.Id);
+            if (article == null)
+            {
+                articles.Add(new ArticlePanier { PizzaId = pizza.Id, PizzaNom = pizza.Name, Prix = pizza.Price, Quantite = quantite });
+            }
+            else
+            {
+                article.Quantite += quantite;
+            }
+        }
+
+        public void SupprimerArticle(int pizzaId)
+        {
+            var article = articles.FirstOrDefault(a => a.PizzaId == pizzaId);
+            if (article != null)
+            {
+                articles.Remove(article);
+            }
+        }
+
+        public decimal ObtenirTotal()
+        {
+            return articles.Sum(a => a.Prix * a.Quantite);
+        }
+
+        public void ViderPanier()
+        {
+            articles.Clear();
+        }
     }
 }
