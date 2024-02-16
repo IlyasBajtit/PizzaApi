80b30972375cc52b323930e203c662e4ce1f150b
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-16 09:28:05 +0100

    feat: add ArticlePanier Models

---
+++ ApiBlazorPizza/Models/ArticlePanier.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiBlazorPizza.Models
+{
+    public class ArticlePanier
+    {
+        public int PizzaId { get; set; }
+        public string PizzaNom { get; set; }
+        public decimal Prix { get; set; }
+        public int Quantite { get; set; }
+    }
+}
